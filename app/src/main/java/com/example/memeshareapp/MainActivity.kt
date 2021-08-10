15f4fcd3b8f061ca99cf65891e4c6631d3d16497
Prince <princedagar71@gmail.com>
2021-08-10 13:49:19 +0530

    Progess Bar

--- app/src/main/java/com/example/memeshareapp/MainActivity.kt
+++ app/src/main/java/com/example/memeshareapp/MainActivity.kt
@@ -2,14 +2,48 @@ package com.example.memeshareapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        LoadMeme()
     }
+    fun LoadMeme(){
+        // Instantiate the RequestQueue.
+        progress_bar.visibility=View.VISIBLE
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://meme-api.herokuapp.com/gimme"
 
-    fun NextMeme(view: View) {}
-    fun ShareMeme(view: View) {}
+        // Request a string response from the provided URL.
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url,null,
+            Response.Listener { response ->
+                val url = response.getString("url")
+
+                Glide.with(this).load(url).into(memeImageView)
+            },
+            Response.ErrorListener {
+                Toast.makeText(this,"Something is Wrong",Toast.LENGTH_LONG).show()
+            })
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest)
+    }
+    fun NextMeme(view: View) {
+        LoadMeme()
+    }
+    fun ShareMeme(view: View) {
+
+    }
 }
