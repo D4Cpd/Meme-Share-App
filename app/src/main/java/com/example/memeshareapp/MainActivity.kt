dc68bcd07f0cbbeb09ad6278b1722f9e2a17b328
Prince <princedagar71@gmail.com>
2021-08-10 14:01:22 +0530

    Progess Bar Complete

--- app/src/main/java/com/example/memeshareapp/MainActivity.kt
+++ app/src/main/java/com/example/memeshareapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.memeshareapp
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ProgressBar
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
@@ -11,6 +13,10 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -31,7 +37,30 @@ class MainActivity : AppCompatActivity() {
             Response.Listener { response ->
                 val url = response.getString("url")
 
-                Glide.with(this).load(url).into(memeImageView)
+                Glide.with(this).load(url).listener(object: RequestListener<Drawable>{
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+
+                        progress_bar.visibility=View.GONE
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+
+                        progress_bar.visibility=View.GONE
+                        return false
+                    }
+                } ).into(memeImageView)
             },
             Response.ErrorListener {
                 Toast.makeText(this,"Something is Wrong",Toast.LENGTH_LONG).show()
